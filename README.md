cf98a51a8127e2bad10b16d28dd661b02e05bda5
Jean-Marc Grimaud <jean-marc.grimaud@st.com>
2018-12-17 14:01:24 +0100

    header update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# kernel
-st21nfc kernel
+# I2C kernel driver for ST21NFC and ST54J_CLF
+st21nfc.c
+
+#SPI kernel driver for ST54J_SE
+st54j_se.c
+
--- st54j_se.c
+++ st54j_se.c
@@ -1,10 +1,18 @@
 /* drivers/nfc/ese/st54j_se.c
- * Copyright (C) 2018 ST Microelectronics S.A.
+ * Copyright (C) 2016 ST Microelectronics S.A.
  * Copyright 2017 Google Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 #include <linux/kernel.h>
 #include <linux/module.h>
